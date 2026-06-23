fc296c864e934b26baab60d59d26f057fbc5ee56
gkaur234 <gurjeet234kaur@gmail.com>
2026-06-23 16:34:11 +0100

    Record the lookup table as a string.

---
+++ encryption/__init__.py
@@ -0,0 +1,91 @@
+from operator import and_
+
+from otree.api import *
+
+
+doc = """
+Encryption Task
+"""
+
+
+class C(BaseConstants):
+    NAME_IN_URL = 'encryption'
+    PLAYERS_PER_GROUP = None
+    NUM_ROUNDS = 3
+    PAYMENT_PER_CORRECT = 0.10
+
+
+class Subsession(BaseSubsession):
+    payment_per_correct = models.CurrencyField()
+    lookup_table = models.StringField()
+    word = models.StringField()
+
+    def setup_round(self):
+        self.payment_per_correct = Currency(C.PAYMENT_PER_CORRECT)
+        self.word="AB"
+        self.lookup_table = "BA"
+
+    @property
+    def lookup_dict(self):
+        lookup = {}
+        for letter in ["A", "B"]:
+            lookup[letter] = self.lookup_table.index(letter) + 1
+        return lookup
+            
+
+def creating_session(subsession):
+    subsession.setup_round()
+
+
+class Group(BaseGroup):
+    pass
+
+
+class Player(BasePlayer):
+    time_for_task = models.IntegerField()
+    response_1 = models.IntegerField()
+    response_2 = models.IntegerField()
+    response_3 = models.IntegerField()
+    response_4 = models.IntegerField()
+    response_5 = models.IntegerField()
+    is_correct = models.BooleanField()
+
+    def check_response(self):
+        self.is_correct = (
+            self.response_1 == self.subsession.lookup_dict[self.subsession.word[0]] and
+            self.response_2 == self.subsession.lookup_dict[self.subsession.word[1]]
+        )
+        if self.is_correct:
+            self.payoff = self.subsession.payment_per_correct
+
+
+# PAGES
+class Intro(Page):
+    @staticmethod
+    def is_displayed(player):
+        return player.round_number == 1
+
+
+class Decision(Page):
+    form_model = "player"
+    form_fields = [
+        "response_1",
+        "response_2",
+    ]
+
+    @staticmethod
+    def before_next_page(player,timeout_happened):
+        player.check_response()
+
+
+class Results(Page):
+    @staticmethod
+    def is_displayed(player):
+        return player.round_number == C.NUM_ROUNDS
+
+
+page_sequence = [
+    Intro,
+    Decision,
+    Results,
+]
